5beee6b0e2c1b8af738c4e890ac4da74c4b83f88
Madison Harris <madison.harris002@student.swic.edu>
2025-11-10 15:28:41 -0600

    Refactor nav link selection and enhance internal link labeling

--- src/main.js
+++ src/main.js
@@ -9,5 +9,14 @@ document.querySelector("#app").innerHTML = `
 const HeaderElement = document.querySelector("main-element");
 console.info(HeaderElement);
 
-const navLinks = document.querySelectorAll(`.navbar a`);
+const navLinks = document.querySelectorAll(".navbar a");
 console.info(navLinks);
+
+const navInternalLinks = document.querySelectorAll('.navbar a[href^="/"]');
+navInternalLinks.forEach((link) => {
+  link.textContent += " (internal)";
+});
+
+document.querySelectorAll("input").forEach((element) => {
+  element.style.outline = "2px solid gold";
+});
